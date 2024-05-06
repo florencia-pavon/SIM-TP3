dfb4931410420a37c6d673686ef774557d03c600
Florencia Pavon <florenciapavonbitar@gmail.com>
2024-05-05 22:10:40 -0300

    front mejorado

--- front.py
+++ front.py
@@ -4,13 +4,13 @@ import tkinter.messagebox as messagebox
 from back import *
 
 
-#HAY QUE REHACER ESTA FUNCION ESTA HECHA SOLO PARA VERIFICAR
+#Funcion que calcula y realiza la tabla de montecarlo
 def simular(probabilidad_1er_tiro, probabilidad_7, probabilidad_8, probabilidad_9, rondas, puntaje_max_1, puntaje_max_2, cantidad_puntos_probabilidad, cantidad_rondas_probabilidad, iteracion_i, iteracion_j):
     # Crear una ventana
     ventana_tabla = tk.Toplevel()
     ventana_tabla.title("Tabla de simulación")
 
-    # Crear el Treeview
+    # Crear la tabla
     tabla = ttk.Treeview(ventana_tabla, columns=("Ronda", "Rnd 1", "Pinos 1ra tirada", "Rnd 2", "Pinos 2da tirada", "Puntos", "Puntos acumulados", "Éxito"), show="headings")
     
     # Configurar encabezados de columnas
@@ -20,7 +20,6 @@ def simular(probabilidad_1er_tiro, probabilidad_7, probabilidad_8, probabilidad_
     # Agregar la tabla a la ventana
     tabla.pack(expand=True, fill="both")
     
-
     puntos_totales = 0
     puntos_ronda_probabilidad = 0
     puntos_ronda = 0
@@ -53,11 +52,9 @@ def simular(probabilidad_1er_tiro, probabilidad_7, probabilidad_8, probabilidad_
                 exito += 1
                 
             puntos_ronda_probabilidad = 0   
-            
-                
+                     
         datos = [r+1, round(rnd_1,2), resultado_1, round(rnd_2,2), resultado_2, puntos_ronda, puntos_totales, exito]
         vector[index] = datos
-        
         
         if iteracion_i <= r+1 < iteracion_i + iteracion_j:
             tabla.insert("", "end", values=datos)
@@ -97,7 +94,9 @@ def cargar_datos():
     
     # Variables para almacenar las probabilidades y puntos
     probabilidades_1er_tiro = [tk.DoubleVar() for _ in range(4)]
-    probabilidades_2do_tiro = [tk.DoubleVar() for _ in range(9)]
+    probabilidad_7 = [tk.DoubleVar() for _ in range(4)]
+    probabilidad_8 = [tk.DoubleVar() for _ in range(3)]
+    probabilidad_9 = [tk.DoubleVar() for _ in range(2)]
     puntos_max_1ra_tirada = tk.IntVar()
     puntos_max_2da_tirada = tk.IntVar()
     rondas = tk.IntVar()
@@ -120,57 +119,69 @@ def cargar_datos():
     tk.Entry(ventana_probabilidades, textvariable=probabilidades_1er_tiro[0]).grid(row=3, column=1)
     tk.Label(ventana_probabilidades, text=8).grid(row=7, column=0)
     tk.Entry(ventana_probabilidades, textvariable=probabilidades_1er_tiro[1]).grid(row=7, column=1)
-    tk.Label(ventana_probabilidades, text=9).grid(row=9, column=0)
-    tk.Entry(ventana_probabilidades, textvariable=probabilidades_1er_tiro[2]).grid(row=9, column=1)
-    tk.Label(ventana_probabilidades, text=10).grid(row=11, column=0)
-    tk.Entry(ventana_probabilidades, textvariable=probabilidades_1er_tiro[3]).grid(row=11, column=1)
+    tk.Label(ventana_probabilidades, text=9).grid(row=11, column=0)
+    tk.Entry(ventana_probabilidades, textvariable=probabilidades_1er_tiro[2]).grid(row=11, column=1)
+    tk.Label(ventana_probabilidades, text=10).grid(row=14, column=0)
+    tk.Entry(ventana_probabilidades, textvariable=probabilidades_1er_tiro[3]).grid(row=14, column=1)
     
     # Posibles cantidades de pinos y probabilidades en el 2do tiro
-    for j, cantidad in enumerate([0, 1, 2, 3, 0, 1, 2, 0, 1], start=2):
+    #Probabilidad del 7
+    for j, cantidad in enumerate([0, 1, 2, 3], start=2):
         tk.Label(ventana_probabilidades, text=cantidad).grid(row=j, column=2)
-        tk.Entry(ventana_probabilidades, textvariable=probabilidades_2do_tiro[j-2]).grid(row=j, column=3)
+        tk.Entry(ventana_probabilidades, textvariable=probabilidad_7[j-2]).grid(row=j, column=3)
+    tk.Label(ventana_probabilidades, text=' ').grid(row=6, column=2)
+    tk.Label(ventana_probabilidades, text=' ').grid(row=6, column=3)
+    #Probabilidad del 8
+    for j, cantidad in enumerate([0, 1, 2], start=7):
+        tk.Label(ventana_probabilidades, text=cantidad).grid(row=j, column=2)
+        tk.Entry(ventana_probabilidades, textvariable=probabilidad_8[j-7]).grid(row=j, column=3)
+    tk.Label(ventana_probabilidades, text=' ').grid(row=10, column=2)
+    tk.Label(ventana_probabilidades, text=' ').grid(row=10, column=3)
+    #Probabilidad del 9
+    for j, cantidad in enumerate([0, 1], start=11):
+        tk.Label(ventana_probabilidades, text=cantidad).grid(row=j, column=2)
+        tk.Entry(ventana_probabilidades, textvariable=probabilidad_9[j-11]).grid(row=j, column=3)
+    tk.Label(ventana_probabilidades, text=' ').grid(row=13, column=2)
+    tk.Label(ventana_probabilidades, text=' ').grid(row=13, column=3)
 
         
-
-    
         
     #Puntos
     for j, puntos in enumerate([7,8,9], start=2):
         tk.Label(ventana_probabilidades, text=puntos).grid(row=j, column=4)
-    for j, puntos in enumerate([8,9], start=6):
+    for j, puntos in enumerate([8,9], start=7):
         tk.Label(ventana_probabilidades, text=puntos).grid(row=j, column=4)
-    tk.Label(ventana_probabilidades, text=9).grid(row=9, column=4)
+    tk.Label(ventana_probabilidades, text=9).grid(row=11, column=4)
     
-    tk.Entry(ventana_probabilidades, textvariable=puntos_max_1ra_tirada).grid(row=11, column=4)
+    tk.Entry(ventana_probabilidades, textvariable=puntos_max_1ra_tirada).grid(row=14, column=4)
     tk.Entry(ventana_probabilidades, textvariable=puntos_max_2da_tirada).grid(row=5, column=4)
-    tk.Entry(ventana_probabilidades, textvariable=puntos_max_2da_tirada).grid(row=8, column=4)
-    tk.Entry(ventana_probabilidades, textvariable=puntos_max_2da_tirada).grid(row=10, column=4)
-    tk.Label(ventana_probabilidades, text="Cantidad de RONDAS").grid(row=1, column=5)
-    tk.Entry(ventana_probabilidades, textvariable=rondas).grid(row=1, column=6)
-    tk.Label(ventana_probabilidades, text="Para calcular la PROBABILIDAD").grid(row=2, column=6)
-    tk.Label(ventana_probabilidades, text="Cantidad de RONDAS").grid(row=3, column=5)
-    tk.Entry(ventana_probabilidades, textvariable=rondas_probabilidad).grid(row=3, column=6)
-    tk.Label(ventana_probabilidades, text="Cantidad de PUNTOS").grid(row=4, column=5)
-    tk.Entry(ventana_probabilidades, textvariable=puntos_probabilidad).grid(row=4, column=6)
-    tk.Label(ventana_probabilidades, text="Valor de i").grid(row=5, column=5)
-    tk.Entry(ventana_probabilidades, textvariable=valor_i).grid(row=5, column=6)
-    tk.Label(ventana_probabilidades, text="Valor de j").grid(row=6, column=5)
-    tk.Entry(ventana_probabilidades, textvariable=valor_j).grid(row=6, column=6)
+    tk.Entry(ventana_probabilidades, textvariable=puntos_max_2da_tirada).grid(row=9, column=4)
+    tk.Entry(ventana_probabilidades, textvariable=puntos_max_2da_tirada).grid(row=12, column=4)
+    tk.Label(ventana_probabilidades, text="Cantidad de Rondas").grid(row=1, column=6)
+    tk.Entry(ventana_probabilidades, textvariable=rondas).grid(row=2, column=6)
+    tk.Label(ventana_probabilidades, text="Para calcular la PROBABILIDAD").grid(row=5, column=6)
+    tk.Label(ventana_probabilidades, text="RONDAS").grid(row=6, column=5)
+    tk.Entry(ventana_probabilidades, textvariable=rondas_probabilidad).grid(row=6, column=6)
+    tk.Label(ventana_probabilidades, text="PUNTOS").grid(row=7, column=5)
+    tk.Entry(ventana_probabilidades, textvariable=puntos_probabilidad).grid(row=7, column=6)
+    tk.Label(ventana_probabilidades, text="Valor de i").grid(row=10, column=5)
+    tk.Entry(ventana_probabilidades, textvariable=valor_i).grid(row=10, column=6)
+    tk.Label(ventana_probabilidades, text="Valor de j").grid(row=11, column=5)
+    tk.Entry(ventana_probabilidades, textvariable=valor_j).grid(row=11, column=6)
 
 
     
     # Botón para validar las probabilidades
-    boton_validar = tk.Button(ventana_probabilidades, text="Validar", command=lambda: validar_ingreso(probabilidades_1er_tiro, probabilidades_2do_tiro, puntos_max_1ra_tirada, puntos_max_2da_tirada, rondas, puntos_probabilidad, rondas_probabilidad, valor_i, valor_j))
+    boton_validar = tk.Button(ventana_probabilidades, text="Validar", command=lambda: validar_ingreso(probabilidades_1er_tiro, probabilidad_7, probabilidad_8, probabilidad_9, puntos_max_1ra_tirada, puntos_max_2da_tirada, rondas, puntos_probabilidad, rondas_probabilidad, valor_i, valor_j))
     boton_validar.grid(row=20, column=20, pady=10)
     
     
-def validar_ingreso(probabilidades_1er_tiro, probabilidades_2do_tiro, puntos_max_1ra_tirada, puntos_max_2da_tirada, rondas, puntos_probabilidad, rondas_probabilidad, valor_i, valor_j):
+def validar_ingreso(probabilidades_1er_tiro, prob_7, prob_8, prob_9, puntos_max_1ra_tirada, puntos_max_2da_tirada, rondas, puntos_probabilidad, rondas_probabilidad, valor_i, valor_j):
     # Obtener los valores ingresados por el usuario
     valores_probabilidades_1er_tiro = [probabilidad.get() for probabilidad in probabilidades_1er_tiro]
-    valores_probabilidades_2do_tiro = [probabilidad.get() for probabilidad in probabilidades_2do_tiro]
-    probabilidad_7 = valores_probabilidades_2do_tiro[0:4]
-    probabilidad_8 = valores_probabilidades_2do_tiro[4:7]
-    probabilidad_9 = valores_probabilidades_2do_tiro[7:]
+    probabilidad_7 = [probabilidad.get() for probabilidad in prob_7]
+    probabilidad_8 = [probabilidad.get() for probabilidad in prob_8]
+    probabilidad_9 = [probabilidad.get() for probabilidad in prob_9]
     puntaje_maximo_1ra_tirada = puntos_max_1ra_tirada.get()
     puntaje_maximo_2da_tirada = puntos_max_2da_tirada.get()
     cantidad_rondas = rondas.get()
